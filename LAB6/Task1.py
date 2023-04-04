0b92fc74d1b76f7d016e64e08cfc6e01efbc00ca
thisisarnabdas <62767878+thisisarnabdas@users.noreply.github.com>
2023-04-04 07:01:33 +0600

    updated answer

--- LAB6/Task1.py
+++ LAB6/Task1.py
@@ -23,5 +23,6 @@ s3 = Student("Tahura", "EEE", 22, 3.01)
 s3.showDetails() 
 print("-----------------------")
 s4 = Student.from_String("Sumaiya-BBA-23-3.96")
-s4.showDetails() 
-
+s4.showDetails()
+print("A class variable is a variable that is shared among all instances of a class. It is defined at the class level and can be accessed by calling it with the class name . On the other hand, an instance variable is a variable whose value is specific to each instance of a class. It is defined within methods and can have different values across different objects ")
+print("An instance method is a method that operates on an instance of a class and has access to its instance variables . On the other hand, a class method is a method that operates on the class itself and does not have access to a particular instance’s variables unless an instance is passed in as a parameter ")
